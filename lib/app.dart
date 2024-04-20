cebfbe84b1ba7949fa5429b37578e7c7259753a7
darktpro <ixidarklord@gmail.com>
2024-04-21 01:58:58 +0200

    ismail 4/21/2024 00.58AM

--- lib/app.dart
+++ lib/app.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:saveit/features/authentication/screens/onboarding.dart';
+import 'package:saveit/features/authentication/screens/welcome/welcome.dart';
+/* import 'package:saveit/features/authentication/controllers/welcome_controller.dart';
+import 'package:saveit/features/authentication/screens/Welcome/welcome.dart';
+import 'package:saveit/features/authentication/screens/onboarding.dart'; */
 
 
 class App extends StatelessWidget {
@@ -10,7 +13,7 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return const GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      home:const OnBoardingScreen(),
+      home: Welcome(),
     );
   }
 }
--- lib/utils/constants/text_strings.dart
+++ lib/utils/constants/text_strings.dart
@@ -1,6 +1,11 @@
 
-class TText{
 
+class TText{
+//Welcome
+    static const String welcometext1= "It's not how much money you make, but how much money you keep, how hard it works for you, and how many ";
+    static const String header= 'SAVE YOUR MONEY';
+
+//onBoarding 
   static const String onBoardText1 = '"It\'s not how much money you make, but how much money you keep, how hard it works for you, and how many generations you keep it for."';
   static const String onBoardText2 = '"It\'s not how much money you make, but how much money you keep, how hard it works for you, and how many generations you keep it for."';
   static const String onBoardText3 = '"It\'s not how much money you make, but how much money you keep, how hard it works for you, and how many generations you keep it for."';
