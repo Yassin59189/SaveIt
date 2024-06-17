895a80e0455513ef1d288ea4b7dc1e8f0c52399d
Yassin59189 <yassin.59189@gmail.com>
2024-06-17 14:28:34 +0100

    firebase setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
 import 'package:saveit/app.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
-
+Future<void> main() async {
   // Todo: Await Native Splash
 
+  // Firebase
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  ).then(
+    (FirebaseApp value) => Get.put(AuthenticationRepository()),
+  );
+
   runApp(const App());
-  
 }
-
-
-
